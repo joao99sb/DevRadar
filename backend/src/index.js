e311eb19797c9688bd55e43216913fa4c8cc2daa
João Victor Dias <joaovictor665@gmail.com>
2020-10-02 11:30:26 -0300

    ,

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3333
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
-mongoose.connect('mongodb+srv://joao99sb:1234@cluster0.sc82m.mongodb.net/testando?retryWrites=true&w=majority',{
+
+const pass = process.env.MONGOOSE_PASS
+
+
+
+mongoose.connect(`mongodb+srv://joao99sb:${pass}@cluster0.sc82m.mongodb.net/testando?retryWrites=true&w=majority`,{
     useNewUrlParser: true,
     useUnifiedTopology:true ,
     useCreateIndex:true
 })
 
-
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
